0c3166194d25c4089b09ccd24e232bb4d4b40571
Rizky Ramadhan <rizky.ramadhan@ruangguru.com>
2022-03-03 13:39:55 +0700

    rollback

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,19 +1,10 @@
 package greetings
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) (string, error) {
-	// If no name was given, return an error with a message.
-	if name == "" {
-		return "", errors.New("empty name")
-	}
-
-	// If a name was received, return a value that embeds the name
-	// in a greeting message.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+	return message
 }
