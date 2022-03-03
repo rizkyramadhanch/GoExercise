258013963842a502267407d2fea6e4689d3d0aa4
Rizky Ramadhan <rizky.ramadhan@ruangguru.com>
2022-03-03 13:20:43 +0700

    change greetings module

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,8 +3,14 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
+	if name == "" {
+		return "", error.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
