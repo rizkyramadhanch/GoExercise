f2ac5c1065f85de72109cd8cb130dcb0fbe27cad
Rizky Ramadhan <rizky.ramadhan@ruangguru.com>
2022-03-03 13:26:57 +0700

    create condition greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,12 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
 	if name == "" {
-		return "", error.New("empty name")
+		return "", errors.New("empty name")
 	}
 
 	// If a name was received, return a value that embeds the name
